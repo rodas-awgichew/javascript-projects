dac141da1590dbd963f975fde3eab43acf57c6cc
Sadorra <rodiawg@gmail.com>
2025-08-11 09:44:32 +0300

    currency exchange updated.

--- Project Assignement/exchange-rate/script.js
+++ Project Assignement/exchange-rate/script.js
@@ -1,19 +1,27 @@
-const curreny_one = document.getElementById("currency-one");
-const curreny_two = document.getElementById("currency-two");
+const currency_one = document.getElementById("currency-one");
+const currency_two = document.getElementById("currency-two");
 const amount_one = document.getElementById("amount-one");
 const amount_two = document.getElementById("amount-two");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
-//API link  https://open.exchangerate-api.com/v6/latest
+
 function calculate(){
-  // your code
-    
+ // fetch currency data using API link
+  fetch('https://open.exchangerate-api.com/v6/latest')
+  .then(res => res.json())
+  .then(data =>{
+    const rate= data.rates[currency_two.value] / data.rates[currency_one.value];
+    rateEl.innerText= `1 ${currency_one.value} =  ${rate.toFixed(2)}  ${currency_two.value}`;
+    amount_two.value = (amount_one.value * rate).toFixed(2);
+  }
+    );
 }
 
+// added event listener
+  
+currency_one.addEventListener('change', calculate);
+currency_two.addEventListener('change', calculate);
+amount_one.addEventListener('input', calculate);
 
-// add evenet listener
 
-swap.addEventListener('click', () =>{
-//  Your code
-})
