602d7e8f9b12dd999b00bff6cb309e4f78d73c1d
Sadorra <rodiawg@gmail.com>
2025-08-12 11:13:50 +0300

    intialized the expense tracker.

--- Project Assignement/expense-tracker/script.js
+++ Project Assignement/expense-tracker/script.js
@@ -18,11 +18,13 @@ const amount = document.getElementById('amount');
 const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 let transactions = localStorageTransactions !== null ? localStorageTransactions : [];
 
+
+// Creates a unique ID for each transaction
 function generateId(){
   return Math.floor( Math.random() * 1000000 );
 }
 
-
+// Function that runs when the form is submitted
 function addTransaction(e) {
   e.preventDefault();
 
@@ -45,6 +47,7 @@ function addTransaction(e) {
   }
 }
 
+//Adds a transaction to the HTML list
 function addTransactionDOM(transaction) {
   const sign = transaction.amount < 0 ? '-' : '+';
   const item = document.createElement('li');
@@ -58,6 +61,7 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// Calculates and updates the balance, income, and expense totals.
 function updateValues() {
   const amounts = transactions.map(t => t.amount);
   const total = amounts.reduce((acc, val) => acc + val, 0).toFixed(2);
@@ -69,19 +73,27 @@ function updateValues() {
   expens.innerText = `$${expense}`;
 }
 
+// Saves the current transactions array to localStorage.
 function updateLocalStorage() {
   localStorage.setItem('transactions', JSON.stringify(transactions)); 
 }
 
+// Removes a transaction by ID and updates everything.
 function removeTransaction(id) {
   transactions = transactions.filter(t => t.id !== id);
   updateLocalStorage();
   init();   
 }
 
+// Clears the list and re-renders all transactions.
 function init() {
   list.innerHTML = '';
   transactions.forEach(addTransactionDOM);
   updateValues(); 
 }
+
+form.addEventListener('submit', addTransaction);
+
+
+init();
 
